5e253c925e5ed95f3aacd505493d1cc56e2c6060
Lisovskii Ivan <dnevanek@gmail.com>
2024-06-20 20:23:26 +0300

    db removed

--- src/routes/llama/handlers.ts
+++ src/routes/llama/handlers.ts
@@ -3,17 +3,17 @@ import ollama from 'ollama'
 
 
 const modelfile = `
-FROM llama3
-SYSTEM "Отвечай только на русском языке. Ты лучшая гадалка на свете. Отвечай развернутыми сообщениями и сообщай судьбу своим друзьям"
+FROM qwen2:0.5b
+SYSTEM "Отвечай только на русском языке. Ты лучшая гадалка на свете. Отвечай развернутыми сообщениями и предсказывай судьбу своим друзьям."
 `
 
-await ollama.create({ model: 'qwe', modelfile: modelfile })
+await ollama.create({ model: 'qwenn', modelfile: modelfile })
 
 
 export async function generateResponse(prompt: string) {
   return new Stream(async(stream) => {
     const response = await ollama.chat({
-      model: 'qwe',
+      model: 'qwenn',
       messages: [{ role: 'user', content: prompt }],
       stream: true
     })
